c5bd1ad5ca5099eff75c73bb9181f13bb27f2e59
jony <jonathan.becker@tiendanube.com>
2021-11-09 21:31:40 -0300

    WebpOptimizer

---
+++ src/Optimizers/WebpOptimizer.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace ImageCompression\Optimizers;
+
+use Exception;
+use ImageCompression\ImageOptimizerInterface;
+use WebPConvert\WebPConvert;
+
+class WebpOptimizer implements ImageOptimizerInterface
+{
+    const WEBP_EXT = 'webp';
+
+    public function optimizeImage(string $pathToImage, string $pathToOutput = null)
+    {
+        try {
+            $pathToImageLevel = $this->getWebpPath($pathToOutput);
+
+            $options = [
+                'png' => [
+                    'encoding' => 'lossy',    // Try both lossy and lossless and pick smallest
+                    'near-lossless' => 100,   // The level of near-lossless image preprocessing (when trying lossless)
+                    'quality' => 95,         // Quality when trying lossy. It is set high because pngs is often selected to ensure high quality
+                    'sharp-yuv' => true,
+                ],
+                'jpeg' => [
+                    'encoding' => 'lossy',     // If you are worried about the longer conversion time, you could set it to "lossy" instead (lossy will often be smaller than lossless for jpegs)
+                    'quality' => 95,          // Quality when trying lossy. It is set a bit lower for jpeg than png
+                    'auto-limit' => true,     // Prevents using a higher quality than that of the source (requires imagick or gmagick extension, not necessarily compiled with webp)
+                    'sharp-yuv' => true,
+                ],
+            ];
+
+            WebPConvert::convert($pathToImage, $pathToImageLevel, $options);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    private function getWebpPath(string $file) : string
+    {
+        $fileParts = pathinfo($file);
+
+        return $fileParts['dirname'] . '/' . $fileParts['filename'] . '.' . self::WEBP_EXT;
+    }
+}
---
+++ tests/WebpOptimizerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ImageCompression;
+
+use ImageCompression\Optimizers\WebpOptimizer;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+
+class WebpOptimizerTest extends TestCase
+{
+    /**
+     * @dataProvider inputAndOutputImageProvider
+     */
+    public function testOptimizeImageWithValidData($pathToImage, $pathToOutput, $expectedPathToOutput)
+    {
+        $options = [
+            'png' => [
+                'encoding' => 'lossy',
+                'near-lossless' => 100,
+                'quality' => 95,
+                'sharp-yuv' => true,
+            ],
+            'jpeg' => [
+                'encoding' => 'lossy',
+                'quality' => 95,
+                'auto-limit' => true,
+                'sharp-yuv' => true,
+            ],
+        ];
+
+        $webpOptimizerMock = Mockery::mock('alias:WebPConvert\WebPConvert');
+        $webpOptimizerMock
+            ->shouldReceive('convert')
+            ->with($pathToImage, $expectedPathToOutput, $options)
+            ->once();
+
+        $defaultImageOptimizer = new WebpOptimizer();
+        $defaultImageOptimizer->optimizeImage($pathToImage, $pathToOutput);
+        Mockery::close();
+    }
+
+    public function inputAndOutputImageProvider() : array
+    {
+        return [
+            ['/tmp/my-directory/image.png', '/tmp/my-directory/image.webp', '/tmp/my-directory/image.webp'],
+            ['/tmp/my-directory/image.png', '/tmp/my-directory/image.png', '/tmp/my-directory/image.webp'],
+        ];
+    }
+}
