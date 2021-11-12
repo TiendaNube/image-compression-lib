bdb5f7ea0624de3a83eedc698fc378025ca1990a
jony <jonathan.becker@tiendanube.com>
2021-11-12 10:46:47 -0300

    converto bool

--- src/DefaultImageOptimizer.php
+++ src/DefaultImageOptimizer.php
@@ -16,15 +16,14 @@ class DefaultImageOptimizer implements ImageOptimizerInterface
 
         $optimizerChain = OptimizerChainFactory::create();
         $optimizerChain->optimize($pathToImage, $pathToOutput);
-        $this->convert($pathToOutput, $pathToOutput);
 
-        return true;
+        return $this->convert($pathToOutput, $pathToOutput);
     }
 
-    private function convert(string $pathToImage, string $pathToOutput)
+    private function convert(string $pathToImage, string $pathToOutput) : bool
     {
         $command = sprintf('convert %s -sampling-factor 4:2:0 -strip -quality 65 %s', $pathToImage, $pathToOutput);
 
-        return shell_exec($command);
+        return shell_exec($command) !== null;
     }
 }
