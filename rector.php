5ab17c2ad40b2dbf7acee9890139cb12fd3aa6b4
Juan G. Arias <juan.arias@tiendanube.com>
2024-06-26 11:24:14 -0300

    Add Rector configuration and setup
    This update establishes a Rector configuration, and sets up a Docker container for PHP Rector. The Rector configuration is designed to ensure strict types and includes paths for source code and test files. The Docker container includes PHP 7.4 and Composer, with the Rector dependency installed.

---
+++ rector.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+use Rector\Set\ValueObject\SetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withSets([SetList::PHP_74,]);
